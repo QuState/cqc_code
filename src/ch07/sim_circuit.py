41dd10af3de97eb084046b0111bdade8c4a4bf01
Logical Guess <logicalguess@gmail.com>
2023-11-15 20:52:27 -0500

    unitary circuit inverse

--- src/ch07/sim_circuit.py
+++ src/ch07/sim_circuit.py
@@ -8,6 +8,8 @@ from ch06.sim_circuit import QuantumTransformation as QuantumTransformation6
 from ch06.sim_circuit import QuantumCircuit as QuantumCircuit6
 from ch05.sim_circuit import Swap
 
+import numpy as np
+
 
 class QuantumRegister(QuantumRegister6):
     pass
@@ -53,6 +55,47 @@ class QuantumCircuit(QuantumCircuit6):
     def c_append_u(self, U, c, q):
         assert(U.shape[0] == U.shape[1] == 2**q.size)
         self.c_unitary(U, c, q.shift)
+
+
+    def inverse(self):
+        qs = [QuantumRegister(size, 'q' if len(self.regs) == 1 else None) for size in self.regs]
+        qc = QuantumCircuit(*qs)
+
+        for tr in self.transformations[::-1]:
+            if isinstance(tr, Swap):
+                qc.swap(tr.i, tr.j)
+                continue
+
+            if tr.name == 'unitary':
+                cs = tr.controls
+                if len(cs) == 0:
+                    qc.unitary(np.conj(tr.gate.transpose()), tr.target)
+                    continue
+                elif len(cs) == 1:
+                    qc.c_unitary(np.conj(tr.gate.transpose()), cs[0], tr.target)
+                    continue
+
+            m = getattr(qc, tr.name)
+            cs = tr.controls
+
+            t = tr.target
+            reg = 0
+            while t >= self.regs[reg]:
+                t = t - self.regs[reg]
+                reg = reg + 1
+
+            if len(cs) == 0:
+                if tr.arg:
+                    m(-tr.arg, qs[reg][t])
+                else:
+                    m(qs[reg][t])
+            elif len(cs) == 1:
+                if tr.arg:
+                    m(-tr.arg, cs[0], qs[reg][t])
+                else:
+                    m(cs[0], qs[reg][t])
+
+        return qc
 
     def run(self):
         for tr in self.transformations:

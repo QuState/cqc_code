898d67cee81664cdef0c1cbd61bd338d2998484d
Logical Guess <logicalguess@gmail.com>
2023-12-04 11:46:50 -0500

    mercury notebooks

---
+++ src/experiments/run_mercury.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from mercury.mercury import main
+
+if __name__ == '__main__':
+    SRC_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    print("SRC", SRC_DIR)
+    os.environ['PYTHONPATH'] = os.environ.get('PYTHONPATH', '') + os.pathsep + SRC_DIR
+    sys.argv.append('run')
+    sys.exit(main())
